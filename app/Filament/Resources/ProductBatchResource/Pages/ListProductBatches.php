dfb3d6e89e31dc78bd966bdb6ee113bfa05067d1
Ram Hussein <ramhussein2002@gmail.com>
2026-05-26 01:24:23 +0300

    add read-only inventory audit resources

---
+++ app/Filament/Resources/ProductBatchResource/Pages/ListProductBatches.php
@@ -0,0 +1,5 @@
+<?php
+namespace App\Filament\Resources\ProductBatchResource\Pages;
+use App\Filament\Resources\ProductBatchResource;
+use Filament\Resources\Pages\ListRecords;
+class ListProductBatches extends ListRecords { protected static string $resource = ProductBatchResource::class; }
---
+++ app/Filament/Resources/StockMovementResource/Pages/ListStockMovements.php
@@ -0,0 +1,5 @@
+<?php
+namespace App\Filament\Resources\StockMovementResource\Pages;
+use App\Filament\Resources\StockMovementResource;
+use Filament\Resources\Pages\ListRecords;
+class ListStockMovements extends ListRecords { protected static string $resource = StockMovementResource::class; }
